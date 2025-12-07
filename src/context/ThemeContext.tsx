a93b92d946fe2b76f1792790fd7042b8814005fb
taqwa <mukminlittaqwa17@gmail.com>
2025-12-07 22:05:09 +0700

    default dark

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -15,7 +15,7 @@ const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  const [theme, setTheme] = useState<Theme>("light");
+  const [theme, setTheme] = useState<Theme>("dark");
   const [isInitialized, setIsInitialized] = useState(false);
 
   useEffect(() => {
