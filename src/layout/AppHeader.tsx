dc8d6d330ae1aed689193f2bc8671d16677983fa
taqwa <mukminlittaqwa17@gmail.com>
2025-12-07 22:13:41 +0700

    first commit

--- src/layout/AppHeader.tsx
+++ src/layout/AppHeader.tsx
@@ -84,7 +84,8 @@ const AppHeader: React.FC = () => {
           </button>
 
           <Link href="/" className="lg:hidden">
-            <Image
+            <span className="Nusantara Monitoring"></span>
+            {/* <Image
               width={154}
               height={32}
               className="dark:hidden"
@@ -97,7 +98,7 @@ const AppHeader: React.FC = () => {
               className="hidden dark:block"
               src="./images/logo/logo-dark.svg"
               alt="Logo"
-            />
+            /> */}
           </Link>
 
           <button
